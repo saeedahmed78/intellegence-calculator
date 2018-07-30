cebe218d4474c2f8a9914166c4b4d111cf07f6f0
saeedahmed78 <saeed01ahmed01@gmail.com>
2018-07-30 12:31:48 +0500

    copy

---
+++ README.md
@@ -0,0 +1,3 @@
+# Intelligence-Calculator
+Randomly calculates your intelligence level
+link : https://ijunaidi.github.io/Intelligence-Calculator/
---
+++ script.js
@@ -0,0 +1,21 @@
+let intMeters = ["Dumb as a Chicken","Imbecile","a Idiot","Below Average","Average","Above Average","Intelligent","Genius","God Gifted","Einstein or something?"]
+let count = 0;
+function calculate() {
+	let userName = document.querySelector("input").value;
+	count++;
+	if (userName === "") {
+	alert("Enter your Name");
+ 	
+}else if ((userName == "Hania" || userName == "hania") && (count <=1)) {
+	document.querySelector(".alert-shownumber").innerHTML = userName + " Your are " + intMeters[6];
+}else if (count <= 1){
+	intMeter = intMeters[Math.floor(Math.random()*intMeters.length)];
+	document.querySelector(".alert-shownumber").innerHTML = userName + " Your are " + intMeter;
+	}
+	else{
+		alert("Please Enter New Name");
+		window.location.replace("index.html");
+	}
+
+}
+
